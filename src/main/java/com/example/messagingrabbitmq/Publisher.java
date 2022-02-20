0cec16cf3edbfe27d113db8516d52d0c8cd5df41
a.dubniak <a.dubniak@profitsoft.ua>
2022-02-20 19:11:42 +0200

    add consumer and publisher

---
+++ src/main/java/com/example/messagingrabbitmq/Publisher.java
@@ -0,0 +1,26 @@
+package com.example.messagingrabbitmq;
+
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.Channel;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+public class Publisher {
+    private final static String QUEUE_NAME = "emailsToSend";
+
+    public static void addEmailIdToSendQueue(String id) {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+
+            channel.queueDeclare(QUEUE_NAME, false, false, false, null);
+            channel.basicPublish("", QUEUE_NAME, null, id.getBytes());
+            System.out.println(" [x] Sent '" + id + "'");
+        } catch (IOException | TimeoutException e) {
+            e.printStackTrace();
+        }
+    }
+}
